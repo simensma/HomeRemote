010376f4dbfb94c3815a778ca056f925cc82ecfb
Simen Fivelstad Smaaberg <simensma@gmail.com>
2017-04-16 20:39:51 -0700

    Added manual arduino detection

--- Server/client.py
+++ Server/client.py
@@ -3,17 +3,18 @@ import warnings
 import serial
 import serial.tools.list_ports
 
-arduino_ports = [
-    p.device
-    for p in serial.tools.list_ports.comports()
-    if 'Arduino' in p.description
-]
-if not arduino_ports:
-    raise IOError("No Arduino found")
-if len(arduino_ports) > 1:
-    warnings.warn('Multiple Arduinos found - using the first')
+ports = serial.tools.list_ports.comports()
 
-ser = serial.Serial(arduino_ports[0], 9600)
+for i,p in enumerate(ports):
+    print i, p
+
+p = input("Select serial port: ")
+
+port = ports[p].device
+
+ser = serial.Serial(port, 9600)
+
+print "Waiting for input..."
 
 while True:
     command = input('Enter command: ')

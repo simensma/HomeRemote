85a48948209200d99b6448316d27131d3062d145
Simen Fivelstad Smaaberg <simensma@gmail.com>
2017-04-16 20:33:01 -0700

    Added arduino serial port detection

--- Server/client.py
+++ Server/client.py
@@ -1,5 +1,19 @@
 import serial
-ser = serial.Serial('/dev/ttyACM0', 9600)
+import warnings
+import serial
+import serial.tools.list_ports
+
+arduino_ports = [
+    p.device
+    for p in serial.tools.list_ports.comports()
+    if 'Arduino' in p.description
+]
+if not arduino_ports:
+    raise IOError("No Arduino found")
+if len(arduino_ports) > 1:
+    warnings.warn('Multiple Arduinos found - using the first')
+
+ser = serial.Serial(arduino_ports[0], 9600)
 
 while True:
     command = input('Enter command: ')
